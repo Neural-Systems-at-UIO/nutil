2922977df54975bbab3f81032c3fb471fc6b753d
sharoncy <s.c.yates@medisin.uio.no>
2020-02-14 14:44:05 +0100

    publish

--- Publish/publish_win/upload_win.cmd
+++ Publish/publish_win/upload_win.cmd
@@ -1,3 +1,3 @@
 del nutil.zip
 7z a nutil.zip nutil
-pscp nutil.zip leuat@www.irio.co.uk:www.irio.co.uk/med/nutil/nutil_win.zip
+#pscp nutil.zip leuat@www.irio.co.uk:www.irio.co.uk/med/nutil/nutil_win.zip
