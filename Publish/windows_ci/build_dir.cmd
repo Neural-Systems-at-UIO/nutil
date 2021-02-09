55dbeff5ce11bd2a9b79cebfc1e357c2315c8c77
Leuat <leuat@irio.co.uk>
2021-02-09 10:36:05 +0100

    CI Test #12

--- Publish/windows_ci/build_dir.cmd
+++ Publish/windows_ci/build_dir.cmd
@@ -7,7 +7,7 @@ xcopy /s %dir%\iconengines nutil\program
 xcopy /s %dir%\platforms nutil\program
 xcopy /s %dir%\*.dll nutil\program
 xcopy /e Publish\source\* nutil
-xcopy Publish\publish_wih\Nutil.cmd nutil\
+xcopy Publish\publish_win\Nutil.cmd nutil\
 REM xcopy 7z.exe nutil\
 xcopy /e Publish\publish_win\dll\* nutil\program
 move nutil\program\nutil.exe nutil\program\nutil_program.exe
