53606cef53496f4b83b475f2d9b0d621220fa3c0
Leuat <leuat@irio.co.uk>
2021-02-09 08:58:03 +0100

    CI Test #10

--- Publish/windows_ci/windows_build.cmd
+++ Publish/windows_ci/windows_build.cmd
@@ -1,11 +1,13 @@
 REM echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 REM set uploadScript=C:\Users\leuat\Documents\GitHub\uploadScript\upload_win_night.cmd
 
 echo "Qmake.."
 qmake Nutil.pro 
 echo "Compiling.."
-jom.exe -f Makefile.Release
+dir %Qt5_DIR%
+%Qt5_DIR%\jom.exe -f Makefile.Release
+
 cd release
 windeployqt.exe .
 
