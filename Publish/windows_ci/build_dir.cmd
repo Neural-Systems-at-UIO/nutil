8e30353f15f58415bedaabcf4af6cbfdddd55b83
Leuat <leuat@irio.co.uk>
2021-02-09 13:58:33 +0100

    Final CI - working

--- Publish/windows_ci/build_dir.cmd
+++ Publish/windows_ci/build_dir.cmd
@@ -10,6 +10,7 @@ xcopy /e Publish\source\* nutil
 xcopy Publish\publish_win\Nutil.cmd nutil\
 xcopy version.txt nutil\program
 xcopy version.txt nutil
+
 REM xcopy 7z.exe nutil\
 xcopy /e Publish\publish_win\dll\* nutil\program
 move nutil\program\nutil.exe nutil\program\nutil_program.exe
