84c2ac5a20e84feea3d797c6b7afc6caa80f4250
Leuat <leuat@irio.co.uk>
2021-02-25 11:58:13 +0100

    CI bug #10

--- Publish/windows_ci/windows_build.cmd
+++ Publish/windows_ci/windows_build.cmd
@@ -1,18 +1,17 @@
 REM echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-REM set uploadScript=C:\Users\leuat\Documents\GitHub\uploadScript\upload_win_night.cmd
 
 echo "Qmake.."
 qmake Nutil.pro 
 echo "Compiling.."
-dir D:\a\nutil\Qt\Tools
 REM dir D:\a\nutil\Qt\Tools\QtCreator /s
 REM jom.exe -f Makefile.Release
 D:\a\nutil\Qt\Tools\QtCreator\bin\jom.exe -f Makefile.Release
 
-cd release
+dir .
+cd Release
 dir .
 windeployqt.exe .
 
-cd ..\Publish\publish_win
+cd Publish\publish_win
 call PublishWindows.cmd
