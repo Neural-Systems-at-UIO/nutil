4e3f9ea7a73dbb2d8f4cbcac054b1cf649418ee0
Leuat <leuat@irio.co.uk>
2021-08-03 13:48:29 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -45,9 +45,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
 'navigation_depth': -1,
 }
