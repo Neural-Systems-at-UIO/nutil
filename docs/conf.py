e1659c3a5644eb8320842587976b6520967cdfb7
Leuat <leuat@irio.co.uk>
2021-08-03 13:46:22 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -39,6 +39,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -46,6 +47,10 @@ exclude_patterns = []
 #
 html_theme = 'alabaster'
 
+#html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+'navigation_depth': -1,
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
