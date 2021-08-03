6157498ec65d0bd72774a8e873219200546f670a
Leuat <leuat@irio.co.uk>
2021-08-03 14:35:16 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -49,10 +49,11 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-'navigation_depth': 4,
-'collapse_navigation': False,
-'sticky_navigation': True,
-}
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
