a22c3bf63bdc442050eb780d5c60f5cc4d55550d
sharoncy <30626642+sharoncy@users.noreply.github.com>
2021-08-03 16:19:40 +0200

    Update conf.py
    Testing

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,6 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
-    'collapse_navigation': False,
     'sticky_navigation': True,
     'navigation_depth': 4,
     'includehidden': True,
