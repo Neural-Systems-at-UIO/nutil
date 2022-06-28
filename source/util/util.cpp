c77e403d18017565186132426a416887dd8426df
Leuat <leuat@irio.co.uk>
2022-06-28 18:57:32 +0200

    New ground truth... bump 3

--- source/util/util.cpp
+++ source/util/util.cpp
@@ -99,9 +99,9 @@ bool Util::CompareIdenticalFiles(QString fa, QString fb)
     }
 
     if (fa.toLower().endsWith(".csv")) {
-        qDebug() << "Comparing CSV files:"+fa+" and "+fb;
+//        qDebug() << "Comparing CSV files:"+fa+" and "+fb;
         if (!QFile::exists(fb)) {
-            qDebug() << "ERROR: Output file missing : "+fb;
+            std::cout << "ERROR: Output file missing : "+fb;
             return false;
         }
         QString a = Util::loadTextFile(fa);
@@ -112,13 +112,13 @@ bool Util::CompareIdenticalFiles(QString fa, QString fb)
         l2.removeAll(QString(""));
 //        qDebug() << l1 << l2;
         if (l1.count()!=l2.count()) {
-            qDebug() << "CSV files do not have the same count of elements";
+            std::cout << "CSV files do not have the same count of elements";
             return false;
         }
 
         for (int i=0;i<l1.count();i++) {
             if (l1[i].trimmed()!=l2[i].trimmed()) {
-                qDebug() << "CSV file not equal for fields "<<l1[i]<<" and " <<l2[i];
+                std::cout << "CSV file not equal for fields "<<l1[i]<<" and " <<l2[i];
                 return false;
             }
         }
