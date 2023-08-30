34c230022f46aa0f8347ad4991ee328aa30b8ec6
sharoncy <s.c.yates@medisin.uio.no>
2023-08-30 14:35:23 +0200

    Add EBRAINS logo

--- docs/conf.py
+++ docs/conf.py
@@ -58,3 +58,8 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_logo = "EBRAINS_logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False
+}
